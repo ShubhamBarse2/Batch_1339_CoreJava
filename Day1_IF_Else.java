cba79a7de20c6e2e9c18d7da6467537b800a4324
ShubhamBarse2 <shubhambarse510@gmail.com>
2026-06-23 18:43:32 +0530

    Add files via upload

---
+++ Day1_IF_Else.java
@@ -0,0 +1,79 @@
+package com.tka;
+
+public class Day1_IF_Else {
+
+	public static void main(String[] args) {
+
+//		if(false){
+//		System.out.println("Hii");	
+//		}
+//
+//		if (45 == 42 || 45 != 42) {
+//			System.out.println("Hii");
+//		} else {
+//			System.out.println("GM");
+//		}
+
+//		String username = "ram@gmail.com";
+//		String password = "Ram@123";
+//
+//		if (username == "ram@gmail.com" && password == "Ram@123") {
+//			System.out.println("Login Successfully ... !");
+//		} else {
+//			System.out.println("INVALID CREDENTIALS ...!");
+//		}
+
+//		Voting Machine : 
+
+//		int age = 21;
+
+		// 18+ // 18
+
+//		if (age >= 18 && age <= 110) {
+//			System.out.println("U r eligible for Voting");
+//		} else {
+//			System.out.println("Ur NOT Eligible for Voting");
+//		}
+
+		// if else ladder statement :
+
+//		Grade system 
+
+		// A(90 to 100) , B(80 to 89), C(70 to 79), D(60 to 69), E(50 to 59), F()
+
+//		char grade = 'B';
+//
+//		if (grade == 'A') {
+//			System.out.println("A Grade Range Between 90 to 100");
+//		} else if (grade == 'B') {
+//			System.out.println("B Grade Range Between 80 to 89");
+//		} else if (grade == 'C') {
+//			System.out.println("C Grade Range Between 70 to 79");
+//		} else if (grade == 'D') {
+//			System.out.println("D Grade Range Between 60 to 69");
+//		} else if (grade == 'E') {
+//			System.out.println("E Grade Range Between 50 to 59");
+//		} else {
+//			System.out.println("Failed Failed Failed ");
+//		}
+
+		
+		
+		
+		if(true) {
+			System.out.println("Hello");
+			if(false) {
+				System.out.println("hii");
+			}else if(true && 56 > 45  ) {
+				System.out.println("bye");
+			}else if(true || 34 > 56) {
+				System.out.println("GN");
+			}
+		}else {
+			System.out.println("GM");
+		}
+		
+
+	}
+
+}
