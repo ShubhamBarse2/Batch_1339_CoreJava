b78a08307a067eb6be42589934aff6e3ad484983
ShubhamBarse2 <shubhambarse510@gmail.com>
2026-06-26 18:30:04 +0530

    Add files via upload

---
+++ Day2SwitchCase.java
@@ -0,0 +1,114 @@
+package com.tka;
+
+public class Day2SwitchCase {
+
+	public static void main(String[] args) {
+
+//		int a = 4;
+
+//		switch (a) {
+//
+//		case 1:
+//			System.out.println("ONE");
+//			break;
+//		case 2:
+//			System.out.println("TWO");
+//			break;
+//		case 3:
+//			System.out.println("Three");
+//			break;
+//		case 4:
+//			System.out.println("Four");
+//	        break;
+//		default:
+//			System.out.println("INVALID INPUT");
+//
+//		}
+
+//		Signal 
+
+//		 clr = "Blue";
+//
+//		switch (clr) {
+//
+//		case "Red":
+//			System.out.println("STOP ..!");
+//			break;String
+//		case "Yellow":
+//			System.out.println("Slow ..!");
+//			break;
+//		case "Green":
+//			System.out.println("GOs ..!");
+//			break;
+//
+//		default:
+//			System.out.println("INVALID CLR");
+//
+//		}
+
+//	Calculator 	
+
+//		char symbol = '%';
+//		int a = 8, b = 4;
+//
+//		switch (symbol) {
+//
+//		case '+':
+//			System.out.println("Addition of " + a + " + " + b + " --> " + (a + b));
+//			break;
+//		case '-':
+//			System.out.println("SUB of " + a + " - " + b + " --> " + (a - b));
+//			break;
+//		case '*':
+//			System.out.println("MUL of " + a + " * " + b + " --> " + (a * b));
+//			break;
+//		case '/':
+//			System.out.println("Div of " + a + " / " + b + " --> " + (a / b));
+//			break;
+//		case '%':
+//			System.out.println("MOD of " + a + " % " + b + " --> " + (a % b));
+//			break;
+//		default:
+//			System.out.println("Invalid Symbol");
+//		}
+
+//		String month = "june";
+//
+//		switch (month) {
+//
+//		case "jan":
+//		case "feb":
+//		case "march":
+//		case "april":
+//			System.out.println("Quater I");
+//			break;
+//		case "may":
+//		case "june":
+//		case "jully":
+//		case "aug":
+//			System.out.println("Quater II");
+//			break;
+//		case "sept":
+//		case "oct":
+//		case "nov":
+//		case "dec":
+//			System.out.println("Quater III");
+//		}
+
+//		32 % 2 == 0 --> EVEN
+
+		int num = 11;
+
+		switch (num % 2) { 	
+
+		case 0:
+			System.out.println("EVEN NUM");
+			break;
+		case 1:
+			System.err.println("ODD NUM");
+
+		}
+
+	}
+
+}
