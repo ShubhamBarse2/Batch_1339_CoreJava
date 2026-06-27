71e14703fc354feecd6ff0bca198072da2981cdc
ShubhamBarse2 <shubhambarse510@gmail.com>
2026-06-27 15:58:26 +0530

    Add files via upload

---
+++ Day3Loop.java
@@ -0,0 +1,80 @@
+package com.tka;
+
+public class Day3Loop {
+
+	public static void main(String[] args) {
+
+//	WAP to print num form 51 to -21
+
+//		51 ,50 , 49 ------ -19 -20 -21
+
+//		 -5 -4 -3 -2 -1  0 1 2 3 4 5
+
+//		for (int i = 51; i >= -21; i++) {
+//			System.out.println(i);
+//		}
+
+		// without condtion i want to create an endless loop ?
+
+//		for (int i = 1; i <= 10; i++);
+//		System.out.println("Hello");
+
+//		for (;;) { 
+//
+//		}
+
+//		while 
+
+//		int i = 1;
+//
+//		while (i <= 50) {
+//			System.out.println("Hello -> " + i);
+//			i++;
+//		}
+
+//		int i = 1;
+//
+//		while (i <= 10) {
+//			System.out.println(i * 250);
+//			i++;
+//		}
+
+//		-567 to -456   -3 -2-1 0     567 456
+
+//		int i = -567;
+//		while(i <= -456) {
+//			System.out.println(i);
+//			i++;
+//		}
+
+//		while (true) {
+//
+//		}
+
+//		-112 to 112
+//		120 to 0
+//		-56 to 57
+
+//		do while
+
+//		int i = 111;
+//		do {
+//			System.out.println(i);
+//			i--;
+//		} while (i >= -111);
+
+//		int i = 1;
+//		do {
+//			System.out.println(i);
+//			i++;
+//		} while (12 == 12);
+//		System.out.println("Hello");
+
+//		while (2 > 0);
+//		System.out.println("Hii");
+//
+//		for (int i = 3; i <= 30; i++);
+//		System.out.println("Hello");
+
+	}
+}
