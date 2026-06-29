1e4a86a9f65ba9ebe7a2fbc895b9df512b6647b1
ShubhamBarse2 <shubhambarse510@gmail.com>
2026-06-29 18:52:09 +0530

    Add files via upload

---
+++ Day4Break_Continue.java
@@ -0,0 +1,98 @@
+package com.tka;
+
+public class Day4Break_Continue {
+
+	public static void main(String[] args) {
+
+//		for (int i = 1; i <= 20; i++) {
+//			if (i == 10) {
+////				System.out.println(i);
+//				break;
+//			}
+//			System.out.println(i);
+//		}
+
+//		10 to -10 bt break 0
+
+//		int i = 10;
+//		while (i >= -10) {
+//			if (i == 0) {
+////				System.out.println(i);
+//				break;
+//			}
+//			System.out.println(i);
+//			i--;
+//		}
+
+//		break :  30 
+//		90 to 29
+
+//		int i = 90;
+//		do {
+//			if (i == 30) {
+//				break;
+//			}
+//			System.out.println(i);
+//			i--;
+//		} while (i >= 29);
+//
+
+//		continue
+//
+//		for (int i = 1; i <= 10; i++) {
+//			if (i == 5) {
+//				continue;
+//			}
+//
+//			System.out.println(i);
+//
+//		}
+
+//		for (int i = 1; i <= 10; i++) {
+//			if (i % 2 == 0) {
+//				continue;
+//			}
+//
+//			System.out.println(i);
+//
+//		}
+
+//		for (int i = 1; i <= 10; i++) {
+//			if (i % 2 == 1) {
+//				continue;
+//			}
+//
+//			System.out.println(i);
+//
+//		}
+
+//		ASCII : 	
+//		American Standard code Information interchange 
+//		int  0 to 5788
+//		 0-9  A-Z   a-b 
+
+//		for (int ch = 45; ch <= 125; ch++) {
+//			System.out.println((char) ch + " -> " + ch);
+//		}
+
+//		digit -> 48 to 57 Capls --> 65 to 90 lowercase 97 to 122
+
+//		char ascii = '1';
+//
+//		if (ascii >= 48 && ascii <= 57) {
+//			System.out.println("DIGIT ... --> " + ascii);
+//		} else if (ascii >= 65 && ascii <= 90) {
+//			System.out.println("UPPERCASE ALPHABETS ... --> " + ascii);
+//		} else if (ascii >= 97 && ascii <= 122) {
+//			System.out.println("LOWERCASE ALPHABETS ... --> " + ascii);
+//		} else {
+//			System.out.println("SPECIAL SYMBOL --> " + ascii);
+//		}
+		
+		
+
+		
+		
+	}
+
+}
