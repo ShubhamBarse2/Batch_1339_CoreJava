2c5e3eef57fac191ef82c1e2cee018a1c96eaa3f
ShubhamBarse2 <shubhambarse510@gmail.com>
2026-06-30 18:58:13 +0530

    Add files via upload

---
+++ Day5_Nested_Loop.java
@@ -0,0 +1,67 @@
+package com.tka;
+
+public class Day5_Nested_Loop {
+
+	public static void main(String[] args) {
+
+//		20 
+
+//		for (int i = 1; i <= 4; i++) { // Outer // rows // 4 1
+//
+//			for (int j = 1; j <= 5; j++) { // Inner // column // 5
+//
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+
+//		for (int i = 1; i <= 4; i++) {
+//
+//			for (int j = 1; j <= i; j++) {
+//
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+
+//		for (int i = 4; i >= 1; i--) {
+//
+//			for (int j = 1; j <= i; j++) {
+//
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+
+//		for (int i = 4; i >= 1; i--) {		
+//			for (int j = 1; j <= 4-i; j++) {
+//				System.out.print(" ");
+//			}
+//			for (int k = 1; k <= i; k++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+
+//		for (int i = 1; i <= 4; i++) {
+//			for (int j = 1; j <= 4 - i; j++) {
+//				System.out.print(" ");
+//			}
+//			for (int k = 1; k <= i * 2-1; k++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+
+//		for (char i = 'A'; i <= 'E'; i++) { // Outer // rows // 4 1
+//
+//			for (char j = 'A'; j <= i; j++) { // Inner // column // 5
+//
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+
+	}
+
+}
