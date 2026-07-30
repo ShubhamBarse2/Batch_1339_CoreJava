4bea18b1e703c1639b3610584a461f5ff62590cd
ShubhamBarse2 <shubhambarse510@gmail.com>
2026-07-30 18:27:13 +0530

    Add files via upload

---
+++ AccessSpecifer&Final/src/com/tka/Demo.java
@@ -0,0 +1,15 @@
+package com.tka;
+
+public class Demo {
+
+	protected int age;
+
+	protected Demo(int a) {
+		System.out.println("this is Cosntr");
+	}
+
+	protected void m1() {
+		System.out.println("this is m1 method");
+	}
+
+}
---
+++ AccessSpecifer&Final/src/com/tka/Demo2.java
@@ -0,0 +1,9 @@
+package com.tka;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		Demo d = new Demo(23);
+		d.m1();
+	}
+}
