dbb5de1f03bd6198dced4309ab6c8315d8d37f7e
ShubhamBarse2 <shubhambarse510@gmail.com>
2026-08-06 19:06:06 +0530

    Add files via upload

---
+++ String_ref_Code/A.java
@@ -0,0 +1,57 @@
+package String_ref_Code;
+
+public class A {
+
+	public static void main(String[] args) {
+
+//		String name = "Ram";
+//		String name1 = "Ram";
+
+//		System.out.println(System.identityHashCode(name));
+//		System.out.println(System.identityHashCode(name1));
+////
+//		System.out.println(name.hashCode());
+//		System.out.println(name1.hashCode());
+//		System.out.println(name == name1); // checked memory address 
+//		System.out.println(name.equals(name1)); // checked content 
+
+//		2nd phase of Stirng 
+
+//		String s = new String("shyam");
+//		String s1 = new String("shyam");
+
+//		System.out.println(System.identityHashCode(s));
+//		System.out.println(System.identityHashCode(s1));
+
+//		System.out.println(s.hashCode());
+//		System.out.println(s1.hashCode());
+//		System.out.println(s == s1);
+//		System.out.println(s.equals(s1));
+
+		// 3rd :
+
+//		String name = "Om";
+//		String name2 = name;
+////		String fname = new String("Om");
+
+//		System.out.println(System.identityHashCode(name));
+//		System.out.println(System.identityHashCode(fname));
+//		System.out.println(name.hashCode());
+////		System.out.println(fname.hashCode());
+//		System.out.println(name2.hashCode());
+//		System.out.println(name == name2);
+//		System.out.println(name.equals(name2));
+
+		// IMP about SCP no modificaiton
+
+//		String fname = "Rahul";
+//		String flName = fname.concat("Patil"); // modify / update // change /
+//
+//		System.out.println(fname.hashCode());
+//		System.out.println(flName.hashCode());
+//		System.out.println(fname == flName);  // 
+//		System.out.println(fname.equals(flName));
+
+	}
+
+}
