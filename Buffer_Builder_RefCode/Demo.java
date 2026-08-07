828bfa8926ea3225634d38ba16ebd52f2d55d513
ShubhamBarse2 <shubhambarse510@gmail.com>
2026-08-07 19:02:34 +0530

    Add files via upload

---
+++ Buffer_Builder_RefCode/Demo.java
@@ -0,0 +1,60 @@
+package Buffer_Builder_RefCode;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+//		String name = "The";
+//
+//		String s = new String("The");
+////		String sss = s.intern();
+//
+//		s = "The";
+//
+//		System.out.println(s.hashCode());
+//		System.out.println(name.hashCode());
+//
+//		System.out.println(s == name);
+
+//		System.out.println(name.equals(name));
+//		System.out.println(name.charAt(0));
+//		System.out.println(name.contains("then"));
+//		System.out.println(name.concat(" Ram"));
+//		System.out.println(name.trim());
+//		System.out.println(name.length());
+//		System.out.println(name.replace("Kiran", "Ram"));
+
+//		String name = "sss";
+//		name.concat("asdfg");
+//
+//		System.out.println(name.concat("234567"));
+
+//		String ww = "asdfghj"; // immutable
+
+		StringBuffer sf = new StringBuffer(ww);   // mutable 
+
+		StringBuilder sb = new StringBuilder("Hello");
+//		StringBuilder ss = sb.append("World");
+////
+//
+//		System.out.println(System.identityHashCode(ss));
+//		System.out.println(System.identityHashCode(sb));
+//
+//		System.out.println(sb.hashCode());
+//		System.out.println(ss.hashCode());
+//
+//		System.out.println(sb == ss);
+
+//		String name = "Ram";
+//		StringBuffer sf = new StringBuffer(name);
+//		StringBuffer fName = sf.append(" yadav");
+////
+//		System.out.println(fName.reverse());
+//		System.out.println(sf.hashCode());
+//		System.out.println(fName.hashCode());
+
+//		System.out.println(fName == name);
+
+	}
+
+}
